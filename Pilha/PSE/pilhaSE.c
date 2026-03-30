b53a096f55c87d31401932f1d34fb5f4e5de0722
Vinicius Moreira Krüger <krugerdv12@gmail.com>
2026-03-30 08:04:30 -0300

    minha pilha

--- Pilha/PSE/pilhaSE.c
+++ Pilha/PSE/pilhaSE.c
@@ -12,7 +12,7 @@ struct stack{
 };
 
 int vazia(struct stack *stk){
-    return stk->stackSize == 0;
+    return (stk->stackSize == 0 || stk == NULL);
 }
 
 struct stack *cria(){
@@ -45,7 +45,53 @@ int insert(int n, struct stack *stk){
     
 }
 
+int pop(struct stack *stk){
+    struct Node *aux = NULL;
+    if(!vazia(stk)){
+        aux = stk->top->under;
+        free(stk->top);
+        stk->top = aux;
+        (stk->stackSize)--;
+        return 1;
+    }
+    return 0;
+}
+
+int first(struct stack *stk){
+    if(stk == NULL || vazia(stk)) return 0;
+    return stk->top->dados;
+}
+
+void reset(struct stack *stk){
+    while(pop(stk));
+}
+
+struct stack *destroy(struct stack *stk){
+    if(stk == NULL) return NULL;
+    reset(stk);
+    free(stk);
+    return NULL;
+}
+
 
 int main(){
+    struct stack *stk = cria();
+    if(stk == NULL){
+        printf("Erro ao criar pilha!\n");
+        return 1;
+    }
+
+    insert(10, stk);
+    insert(20, stk);
+    insert(30, stk);
+
+
+    while(!vazia(stk)){
+        printf("Tamanho da pilha %d\n", stk->stackSize);
+        printf("%d\n", first(stk));
+        pop(stk);
+    }
+
+    destroy(stk);
 
 }
